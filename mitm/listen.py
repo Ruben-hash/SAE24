ecdce1828843fe048d35e266ef2a1c9b65bcf006
Ruben Preti- -Levy <58661892+Ruben-hash@users.noreply.github.com>
2023-06-05 15:37:42 +0200

    fghjk

--- mitm/listen.py
+++ mitm/listen.py
@@ -7,48 +7,31 @@ import time
 from datetime import datetime
 import json
 
-if len(sys.argv) >= 3:
-	print("Erreur d'utilisation du script")
-	exit()
+if len(sys.argv) != 3:
+    print("Erreur d'utilisation du script")
+    exit()
 
-nb_sec = sys.argv[1]
+nb_sec = int(sys.argv[1])
 ip_client = sys.argv[2]
 
-Methode = []
-Chemin = []
-Version = []
-Date = []
 content = []
 
 def check(p):
-	if HTTPRequest in p and p[IP].src == ip_client:
-		req = p[HTTPRequest]
-		
-		content.append(req.Method.decode("utf-8")) 
-		content.append(req.Path.decode("utf-8"))
-		content.append(req.Http_Version.decode("utf-8"))
-		content.append(str(datetime.now()))
-		if req.Method.decode("utf-8") == "POST":
-			content.append(str(req.payload.load.decode("utf-8")))
-	datae = {
-		"methode":"",
-		"chemin":"",
-		"version":"",
-		"date":"",
-		"contenu":""
-	}
-	for data in content:
-		datae.append({
-		"methode":data[0],
-		"chemin":data[1],
-		"version":data[2],
-		"date":data[3],
-		"contenu":data[4]
-	})	
-	with open("Résultat.json", "w", encoding="utf-8") as filecontent:
-            # Ajout des informations du dictionnaire data dans le fichier json
-            json.dump(data, filecontent, indent=4)
+    if HTTPRequest in p and p[IP].src == ip_client:
+        req = p[HTTPRequest]
+        
+        content.append({
+            "methode": req.Method.decode("utf-8"),
+            "chemin": req.Path.decode("utf-8"),
+            "version": req.Http_Version.decode("utf-8"),
+            "date": str(datetime.now())
+        })
 
-sniff(prn=check, timeout=int(nb_sec))
+        if req.Method.decode("utf-8") == "POST":
+            content[-1]["contenu"] = req.payload.load.decode("utf-8")
 
+    with open("Résultat.json", "w", encoding="utf-8") as filecontent:
+        # Écriture du contenu dans le fichier JSON
+        json.dump(content, filecontent, indent=4)
 
+sniff(prn=check, timeout=nb_sec)
